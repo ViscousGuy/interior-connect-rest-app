ed504c43b0f325343b225a85f92e5ba4039cffd9
Kool-Cool <omgrabpoint@gmail.com>
2024-05-07 01:15:31 +0530

    furniture structures updated
    changing structure as per new schema
    
    ( refer .env.example)

--- models/furniture.go
+++ models/furniture.go
@@ -3,21 +3,29 @@ package models
 import "github.com/astaxie/beego/orm"
 
 type Furniture struct {
-    FurnitureId     int     `orm:"auto;column(furniture_id)"` // Note the field name change
-    Name            string  `orm:"size(100)"` 
-    Type            string  `orm:"size(50)"`
-    Style           string  `orm:"size(50)"`
-    Description     string  `orm:"type(text)"`   
-    Dimensions      string  `orm:"size(50)"` 
-    Price           float64 `orm:"digits(10);decimals(2)"`
-    PrimaryImagePath string `orm:"column(primary_image_path);size(255)"` 
-    ContractorId    int     `orm:"column(contractor_id)"` 
+    Id               int     `orm:"auto"`
+    FurnitureTypeId  int
+    RoomTypeId       int
+    Name             string  `orm:"size(100)"`
+    Description      string  `orm:"type(text)"`
+    Dimensions       string  `orm:"size(50)"`
+    Price            float64 `orm:"digits(10);decimals(2)"`
+    ContractorId     int
+    Slug             string  `orm:"size(255)"`
+    Display          bool
 }
+
 
 func (f *Furniture) TableName() string {
     // Explicitly specifying the table name as "Furniture"
-    return "Furniture" 
+ 
+    
+    // Our databse has "furniture" (with lower case)
+
+    return "furniture"   // changing it into lowercase
 }
+
+
 
 func init() {
     orm.RegisterModel(new(Furniture))
